55815ce678a176870feba358adb12634706c293e
Andy Bartholomew <andy.bartholomew@airbnb.com>
2016-06-01 14:39:00 -0700

    add ReproModels framework

--- PodUpgradeOrNot/ViewController.swift
+++ PodUpgradeOrNot/ViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import ReproModels
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    TestModels()
   }
 
   override func didReceiveMemoryWarning() {
---
+++ ReproModels/ReproModels/TestFile.swift
@@ -0,0 +1,13 @@
+//
+//  TestFile.swift
+//  ReproModels
+//
+//  Created by Andy Bartholomew on 6/1/16.
+//  Copyright © 2016 Airbnb. All rights reserved.
+//
+
+import Foundation
+
+public func TestModels() {
+  print("hey")
+}
